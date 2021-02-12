69109f2fff38aefc664a6d96a8cd71559b2c90a9
LifeXplorer <a.l.vinogradov1986@gmail.com>
2021-02-12 12:55:04 +0300

    add exception handler

---
+++ src/main/java/com/itechart/pathfinder/exception/CustomExceptionsHandler.java
@@ -0,0 +1,16 @@
+package com.itechart.pathfinder.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomExceptionsHandler {
+
+    @ExceptionHandler(value = {CityNotFoundException.class, PathNotFoundException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleNotFoundExceptions(Exception ex) {
+        return ex.getMessage();
+    }
+}
